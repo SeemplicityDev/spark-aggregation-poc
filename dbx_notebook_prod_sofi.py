ba54f50dc54357d4659728821f667041b4fda900
eranmoscovici <eran@seemplicity.io>
2025-08-26 16:15:34 +0300

    add sofi notebook

---
+++ dbx_notebook_prod_sofi.py
@@ -0,0 +1,23 @@
+import sys
+import os
+
+# Add the src directory to Python path
+sys.path.append("/Workspace/Users/eran@seemplicity.io/spark-aggregation-poc/src")
+
+from spark_aggregation_poc.config.config import Config
+from spark_aggregation_poc.main import run_aggregation_from_dbx
+
+
+config: Config = Config(postgres_url="jdbc:postgresql://vpce-0ab78c002b187f211-1l5gyxsr.vpce-svc-0ec8249541aa206d0.eu-central-1.vpce.amazonaws.com:54320/postgres?currentSchema=sofi",
+                        postgres_properties={
+                            "user": "postgres",
+                            "password": "99PKFxw!AoZDsfe6",
+                            "driver": "org.postgresql.Driver"
+                        },
+                        is_databricks = True
+)
+# Cell 1: Monitor cluster configuration
+print("=== Cluster Configuration ===")
+print(f"Executor memory: {spark.conf.get('spark.executor.memory')}")
+
+run_aggregation_from_dbx(spark, config=config)

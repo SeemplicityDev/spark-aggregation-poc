d6e76ed86f9d74031e78928a1154f8f4efc958ea
eranmoscovici <eran@seemplicity.io>
2025-09-04 12:29:12 +0300

    read * from findings

--- src/spark_aggregation_poc/data/raw_join_query1_unilever.sql
+++ src/spark_aggregation_poc/data/raw_join_query1_unilever.sql
@@ -7,6 +7,6 @@ SELECT
     findings.fix_subtype as fix_subtype,
     findings.fix_id as fix_id,
     findings.fix_type as fix_type,
- datasource_id, datasource_definition_id, title, "source", finding_id, original_finding_id, region, created_time, discovered_time, due_date, last_reported_time, original_status, time_to_remediate, category, sub_category, cloud_provider, cloud_account, cloud_account_friendly_name, dictionary_category, dictionary_sub_category, rule_id, resource_reported_not_exist,   image_id, scan_id, editable, reopen_date,   fix_vendor_id,  rule_type, rule_family, last_collected_time
+ datasource_id, datasource_definition_id, title, "source", original_finding_id, region, created_time, discovered_time, due_date, last_reported_time, original_status, time_to_remediate, category, sub_category, cloud_provider, cloud_account, cloud_account_friendly_name, dictionary_category, dictionary_sub_category, rule_id, resource_reported_not_exist,   image_id, scan_id, editable, reopen_date,   fix_vendor_id,  rule_type, rule_family, last_collected_time
 FROM findings
 WHERE findings.package_name IS NOT NULL
